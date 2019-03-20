ead4dfd5dc918e7506fea48293b893c197e6e46c
MrTechnocrat <anthony.lawson@mrtechnocrat.com>
2019-03-20 18:43:35 -0400

    2nd Push

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+let PORT = process.env.PORT || 3000;
+
+// Create application / x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+// parse some custom thing into a buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+
+// Parse and HTML body into a string.
+app.use(bodyParser.text({ type: 'text/html' }));
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+	console.log('Server started on PORT: ', PORT);
+});
