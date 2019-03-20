035a145bbe3b459690242cf5d30328a3c4d79f3b
MrTechnocrat <anthony.lawson@mrtechnocrat.com>
2019-03-19 22:20:14 -0400

    1st push

---
+++ app/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+	{
+		name: 'Peter Parker',
+		photo: 'https://anfieldindex.com/wp-content/uploads/2016/12/Ultimate-Peter-Parker-Spider-Man-Haircut.jpg',
+		scores: [ '3', '4', '5', '2', '5', '4', '4', '2', '5', '3' ]
+	},
+
+	{
+		name: 'Nick Furry',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoIB01SaQmVm1SoD6RkY5MkZCjyvpDzTGcd0Livl66hflAgWBTwg',
+		scores: [ '2', '3', '2', '1', '4', '2', '1', '2', '2', '1' ]
+	},
+
+	{
+		name: 'Steve Rodgers',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTadfe1CGbz3SPtRo8agtxzaNL3a822b5mLHu-OjcrKMNtGj9rLyA',
+		scores: [ '4', '4', '2', '2', '2', '4', '5', '2', '4', '2' ]
+	},
+
+	{
+		name: 'Bruce Banner',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUao75jDpQxKvuTy8QmP_WmDqBr2EB2geKKFD5XNGBXyDvDgzi4Q',
+		scores: [ '4', '4', '4', '4', '5', '2', '4', '4', '3', '4' ]
+	},
+
+	{
+		name: 'Tony Stark',
+		photo:
+			'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/syfywire_blog_post/2018/10/avengers-tony-stark-iron-man-movie-hd-wallpaper-2880x1800-1920x1080.jpg',
+		scores: [ '3', '4', '1', '5', '3', '5', '4', '2', '4', '2' ]
+	}
+];
+
+module.exports = friends;
